b75b2d31ceb846eaef84e5d24fb9f9fe5f969283
Iaroslav Os <osmnbx@gmail.com>
2023-08-08 12:25:55 +0300

    Update main.go

--- tests/abcTests/main.go
+++ tests/abcTests/main.go
@@ -1,12 +1,17 @@
 package main
 
 import f "fmt"
+import "runtime"
 
 func divByZero(a int, b int) int {
 	return a / b
 }
 
 func main() {
+	runtime.GOMAXPROCS(2)
+	cpus := runtime.NumCPU()
+	f.Println(cpus)
+
 	defer func(){
 		if r := recover(); r != nil {
 			f.Printf("шось не так: %s\n", r)
