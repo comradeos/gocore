d5748eda79b18a1add632d876bc26fd4203b71bf
Iaroslav Os <osmnbx@gmail.com>
2023-08-22 14:42:41 +0300

    Update 033SimpleWebServer.go

--- go003Basics/033SimpleWebServer.go
+++ go003Basics/033SimpleWebServer.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt",
-	// "net/http"
+	"fmt"
+	"net/http"
 )
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>home</h1>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>about</h1>")
+}
 
 func main() {
 	fmt.Println("Simple Web Server")
-	// http.HandleFunc("/", func(w *http.))
+	
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+
+	http.ListenAndServe(":89", nil)
 }
 
