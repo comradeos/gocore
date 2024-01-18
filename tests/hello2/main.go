692741155d526c0348cfa098f951be67358eb43c
Iaroslav Os <osmnbx@gmail.com>
2024-01-18 21:02:46 +0200

    updated

---
+++ tests/hello2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"hello2/modules"
+)
+
+func main() {
+	fmt.Println("Hello, EnvTest Go!");
+	modules.MyLib();
+	modules.MyLib2();
+}
---
+++ tests/hello2/modules/myLib2.go
@@ -0,0 +1,7 @@
+package modules
+
+import "fmt"
+
+func MyLib2() {
+	fmt.Println("MyLib2 Go!")
+}
