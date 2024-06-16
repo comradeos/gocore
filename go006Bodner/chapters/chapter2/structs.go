52944b66f9b1fe0a0a92ad91c2bdc2f3a6eb1066
Iaroslav Os <osmnbx@gmail.com>
2024-06-16 15:39:53 +0300

    Update structs.go

--- go006Bodner/chapters/chapter2/structs.go
+++ go006Bodner/chapters/chapter2/structs.go
@@ -5,4 +5,22 @@ import "fmt"
 func Structs() {
 	Line()
 	fmt.Println("Structs:")
+
+	type firstPerson struct {
+		age  int
+		name string
+	}
+
+	type secondPerson struct {
+		age  int
+		name string
+	}
+
+	var v1p1 firstPerson
+	v1p1.name = "A"
+	v1p1.age = 1
+
+	var v2p1 secondPerson
+	v2p1.name = "a"
+
 }
