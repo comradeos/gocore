bd0d42215a61f8134f3d202470ee3cace8698d06
Iaroslav Os <osmnbx@gmail.com>
2024-10-17 16:35:18 +0300

    Update main.go

--- tests/checksum/main.go
+++ tests/checksum/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	data := []byte{0xAA, 0x03, 0xFF}
+	data := []byte{0xAA, 0x03, 0x1A}
 	checksum := GetCheckSum(data)
 	prettyPrint(checksum)
 }
@@ -13,23 +13,23 @@ func GetCheckSum(data []byte) byte {
 
 	fmt.Printf("SUM: %d\n", sum)
 
-    for _, b := range data[1:] {
+	for _, b := range data[1:] {
 		fmt.Printf("%d + %d = ", sum, b)
 		sum += b
 		fmt.Printf("%d\n", sum)
-    }
+	}
 
 	result := byte(0 - sum)
 
 	fmt.Printf("0 - %d = %d\n", sum, result)
 
-    return result
+	return result
 }
 
 func prettyPrint(b byte) {
 	fmt.Printf("\nDecimal: %d | Hexadecimal: 0x%02X\n\n", b, b)
 }
 
-// Модульная арифметика: При вычитании 0 - 172 получаем -172. 
+// Модульная арифметика: При вычитании 0 - 172 получаем -172.
 // Чтобы привести этот результат к допустимому диапазону byte, добавляем 256:
 // 0 − 172 + 256 = 84
