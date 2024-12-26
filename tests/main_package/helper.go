0ee308968904126d71e0581ac5609b96e418a392
Iaroslav Os <osmnbx@gmail.com>
2024-12-26 14:22:27 +0200

    updated

---
+++ tests/main_package/helper.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hello1() {
+	fmt.Println("Hello")
+}
---
+++ tests/main_package/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello rmain")
+	hello1()
+}
+
+
+
+
+// go run main.go helper.go
